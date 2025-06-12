ae6164c08aa32b92189f7b911949d54719fe23f8
Jaehyun Kim <passiontom@naver.com>
2025-06-12 11:04:43 -0400

    ✅ 6/12 | 해시 저장/조회 기능 구현 및 테스트 완료

--- app/routes/hash_router.py
+++ app/routes/hash_router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 import hashlib
+from app.services.blockchain_service import contract
+
 
 router = APIRouter()
 
@@ -13,3 +15,19 @@ async def generate_hash(payload: HashInput):
     data_str = payload.model + str(payload.output)
     hash_value = hashlib.sha256(data_str.encode()).hexdigest()
     return {"hash": hash_value}
+
+@router.get("/count")
+def get_count():
+    try:
+        count = contract.functions.getCount().call()
+        return {"count": count}
+    except Exception as e:
+        return {"error": str(e)}
+
+@router.get("/get/{index}")
+def get_hash(index: int):
+    try:
+        hash_value = contract.functions.getHash(index).call()
+        return {"index": index, "hash": hash_value}
+    except Exception as e:
+        return {"error": str(e)}
